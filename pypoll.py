c729929c9d4e950d113bb1e663bc54ca282fc19c
Justin Berry <justinberry@Juberr.local>
2020-11-02 10:51:29 -0500

    finished poll analysis

--- pypoll.py
+++ pypoll.py
@@ -12,6 +12,14 @@ file_to_load = os.path.join("Desktop","UofT Bootcamp","Module 3 - Python","resou
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+candidate_options = []
+
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -23,5 +31,34 @@ with open(file_to_load) as election_data:
     rowcount = 0
     for row in file_reader:
         rowcount +=1
-        print(row)
-    print(rowcount)
+        
+        candidate_name = row[2]
+        
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
+
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+
+        vote_perc = float(votes) / float(rowcount) * 100
+
+        if (votes > winning_count) and (vote_perc > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_perc
+            winning_candidate = candidate_name
+
+
+        print(f"{candidate_name}: {vote_perc:.1f}% ({votes:,})\n")
+    
+    winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+    print(winning_candidate_summary)
+
+
